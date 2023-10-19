f03a1c096eff72ae8e19a77ea833b29ac23af8aa
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-10-19 17:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+         this.name= name;
+         this.age = age;
+         
+      }
+      get name(){
+         return this._name;
+      }
+      set name(value){
+         this._name=value;
+      }
 
-class Student extends Person {}
+      get age(){
+         return this._age;
+      }
+      set age(value){
+         this._age=value;
+      }
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+   
+      constructor(){
+         super("Kritika",28);
+         
+      }
+      study(){
+         console.log(`${name} is studying`);
+      }
+   }
+
+   let a = new Student();
+
+   a.study();
+
+  
+   
+   class Teacher extends Person {
+     
+      constructor(){
+         super("Kritika",28);
+         
+      }
+
+      teach(){
+         console.log(`${name} is teaching`);
+      }
+   }
+
+   let b = new Teacher();
+
+   b.teach();
 
 // Do not change the code below this line
 window.Person = Person;
